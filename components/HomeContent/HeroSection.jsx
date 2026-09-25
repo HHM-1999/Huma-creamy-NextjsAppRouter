a10adb9b1009675cb6e91915ced82ca16de7d787
hhm-1999 <hhm639069@gmail.com>
2026-09-25 20:04:44 +0600

    header fix

--- components/HomeContent/HeroSection.jsx
+++ components/HomeContent/HeroSection.jsx
@@ -1,33 +1,9 @@
-// "use client";
-
-// import React from "react";
-// import IceCreamAnimate from './IceCreamAnimate'
-
-// export default function Hero() {
-//   return (
-//     <section className="container-fluid bg-light py-5">
-//       <div className="row align-items-center">
-//         <div className="col-lg-6 text-center text-lg-start px-5">
-//           <h1 className="display-4 fw-bold">Delicious Ice Cream</h1>
-//           <p className="lead mt-3">
-//             Cool off with our handmade, creamy treats in every flavor imaginable.
-//           </p>
-//           <button className="btn btn-primary btn-lg mt-3">Order Now</button>
-//         </div>
-//         <div className="col-lg-6">
-//           <IceCreamAnimate />
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-import React from "react";
 
 const Hero = () => {
   return (
     <section className="hero-section">
       <div className="container">
-        <div className="row align-items-center ">
+        <div className="row">
 
           {/* Left Content */}
           <div className="col-lg-6 col-md-7">
